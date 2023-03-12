9f707f61f67dfe3077825394ec4d0bb246755652
Tapan Kumar Barik <55124761+TapanKumarBarik@users.noreply.github.com>
2023-03-12 09:17:30 +0530

    Day 1

---
+++ 100 Days of Python/Day 1/Code/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator ..")
+user_name=input("What is your name ? :\n")
+pet_name=input("What is your pet name ? :\n")
+print("one possible band name could be "+user_name+" "+pet_name)
--- 100 Days of Python/Day 1/Code/hello_world.py
+++ 100 Days of Python/Day 1/Code/hello_world.py
@@ -1,2 +1,17 @@
+
+# Date 11th March 2023
+# Author Tapan kumar Barik
+
+#print 
 print("Hello")
-print("Hello World \nHello World \nHello World ")
+
+# \n it creates a new line
+print("Hello World \nHello World \nHello World \n\n")
+
+#String manipulation
+print("Tapan "+"Kumar "+"Barik")
+
+
+# input()
+my_name=input("What is your name ? ")
+print("Ok i am printing my name and it is : " + my_name)
